a3a332cadd6a5de0a81f48c4cb73f772f6acb940
ben@benatkin.com <ben@benatkin.com>
2010-06-14 19:14:56 -0600

    hard-coded a route for hashrocket.

--- anagram.rb
+++ anagram.rb
@@ -4,6 +4,11 @@ get '/' do
   redirect '/anagram-animation/a-giant-airman-moan', 307
 end
 
+get '/hashrocket' do
+  @phrase, @anagram = 'hashrocket', 'hot hackers'
+  erb :index
+end
+
 get %r{/([\w-]+)/([\w-]+).*} do
   @phrase, @anagram = params['captures'].map {|s| s.tr('-', ' ')}
   erb :index
