550491b7d916a8330b0f9db0fdd01665bed221ae
ben@benatkin.com <ben@benatkin.com>
2010-06-14 23:58:33 -0600

    fixed routing error (need tests!)

--- anagram.rb
+++ anagram.rb
@@ -4,18 +4,18 @@ get '/' do
   redirect '/anagram-animation/a-giant-airman-moan', 307
 end
 
-get %r{/([\w-]+)} do
+get %r{/([\w-]+)/([\w-]+).*} do
+  @phrase, @anagram = params['captures'].map {|s| s.tr('-', ' ')}
+  erb :index
+end
+
+get %r{/([\w-]+).*} do
   @phrase = params['captures'][0].tr('-', ' ')
   @anagram = {
     'hashrocket' => 'hot hackers',
     'benjamin atkin' => 'beatnik ninja',
     'clenching thin topsoil' => 'linchpin technologist'
   }[@phrase]
-  erb :index
-end
-
-get %r{/([\w-]+)/([\w-]+).*} do
-  @phrase, @anagram = params['captures'].map {|s| s.tr('-', ' ')}
   erb :index
 end
 
